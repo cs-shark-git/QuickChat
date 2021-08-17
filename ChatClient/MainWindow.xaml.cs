be5d0d1fc576cd7f81c175f3972f8d6ed872743b
CS-Shark-git <cs-shark.git@mail.ru>
2021-08-17 11:56:16 +0300

    fixed buttons and subscribed event handler on exit button, realized exit from the app

--- ChatClient/MainWindow.xaml.cs
+++ ChatClient/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ChatClient
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
