24c3687fb8d1e4e01904d24b4a1b506632b5b295
CS-Shark-git <cs-shark.git@mail.ru>
2021-09-11 17:28:28 +0300

    ConnectDialogViewModel and ViewModel fix

--- ChatClient/ViewModels/Base/ViewModel.cs
+++ ChatClient/ViewModels/Base/ViewModel.cs
@@ -26,6 +26,7 @@ namespace ChatClient.ViewModels.Base
         public bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
         {
             if(Equals(field, value)) return false;
+            field = value;
             OnPropertyChanged(PropertyName);
             return true;
         }
---
+++ ChatClient/ViewModels/ConnectDialogViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ChatClient.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.ViewModels
+{
+    class ConnectDialogViewModel : ViewModel
+    {
+
+        private string _adress;
+        private string _nickName;
+        public string Adress
+        {
+            get => _adress;
+            set => Set(ref _adress, value);
+        }
+
+        public string NickName
+        {
+            get => _nickName;
+            set => Set(ref _nickName, value);
+        }
+
+        public ConnectDialogViewModel()
+        {
+            _adress = "dss";
+            _nickName = "ds3F5DDSSDFGfews";
+        }
+    }
+}
