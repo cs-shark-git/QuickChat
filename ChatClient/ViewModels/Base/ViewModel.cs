e7b1df819d5ae522c5ed47122439e5fc67ab2a41
CS-Shark-git <cs-shark.git@mail.ru>
2021-09-11 13:35:43 +0300

    Base class for ViewModels

---
+++ ChatClient/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.ViewModels.Base
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            if(PropertyChanged is null) return;
+
+            var invocationList = PropertyChanged.GetInvocationList();
+            foreach(var action in invocationList)
+            {
+                action.DynamicInvoke(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+
+        public bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if(Equals(field, value)) return false;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+    }
+}
