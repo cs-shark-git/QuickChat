0e11d3cb23dbd6cfc3eda3df30df3e169406908f
CS-Shark-git <cs-shark.git@mail.ru>
2021-09-11 20:27:48 +0300

    ChatViewModel

---
+++ ChatClient/ViewModels/ChatViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ChatClient.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.ViewModels
+{
+    class ChatViewModel : ViewModel
+    {
+        private string _title = "QuickChat: v 0.0.1";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        public ChatViewModel()
+        {
+
+        }
+    }
+}
--- ChatClient/ViewModels/MainWindowViewModel.cs
+++ ChatClient/ViewModels/MainWindowViewModel.cs
@@ -11,14 +11,6 @@ namespace ChatClient.ViewModels
 {
     class MainWindowViewModel : ViewModel
     {
-        private string _title;
-
-        public string Title
-        {
-            get => _title;
-            set => Set(ref _title, value);
-        }
-
         public ICommand OpenConnectDialogCommand { get; }
 
         private void OpenConnectDialogCommandExecute(object p) => new ConnectDialog().ShowDialog();
@@ -27,7 +19,6 @@ namespace ChatClient.ViewModels
 
         public MainWindowViewModel()
         {
-            _title = "QuickChat: v 0.0.1";
             OpenConnectDialogCommand = new RelayCommand(OpenConnectDialogCommandExecute, OpenConnectDialogCommandCanExecute);
         }
     }
